f7a2616c9f32e822a0f5b381511aa2dcc261943b
holland29 <epoitrr@gmail.com>
2024-01-08 14:39:37 +0900

    20240108

---
+++ 기술면접/js.js
@@ -0,0 +1,9 @@
+//! 비동기, 동기 설명
+//! promise, async, await 차이 설명
+//! 호이스팅과 발생하는 이유는?
+//! var, let, const 차이 설명
+
+// 깊은 복사 vs 얕은 복사 설명
+// map, forEach, reduce 설명
+// rest 연산자, spread 연산자 설명
+// es6에서 신규 기능 아는대로 설명해보기
